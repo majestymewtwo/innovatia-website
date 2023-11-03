6140cd39d266436100815b93b794e5b7f941b21a
majestymewtwo <majestymewtwo@gmail.com>
2023-11-03 11:22:19 +0530

    member card font change

--- components/team/MemberCard.js
+++ components/team/MemberCard.js
@@ -2,20 +2,20 @@
 import React from 'react';
 import Lottie from 'lottie-react';
 import './MemberCard.css';
+import { Linkedin } from "lucide-react";
 
 function MemberCard(props) {
   const { name, role, animationData } = props;
 
   return (
     <div className='homeContainer md:col'>
-      <div className='profile-card'>    
+      <div className='profile-card'>
         <div className='img'>
           <Lottie animationData={animationData} className='' />
         </div>
         <div className='caption'>
-          <h3 className='text-white'>{name}</h3>
-          <p className='text-white'>{role}</p>
-          <div className='social-links'></div>
+          <h3 className='text-white font-black'>{name}</h3>
+          <p className='text-white font-light'>{role}</p>
         </div>
       </div>
     </div>
