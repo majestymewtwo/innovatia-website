adbaae0e5e9d83ca05717f87bbbc8d7788f39f10
majestymewtwo <majestymewtwo@gmail.com>
2023-11-04 17:38:06 +0530

    events page responsive

--- app/events/page.js
+++ app/events/page.js
@@ -10,8 +10,8 @@ export default function Events() {
 
   return (
     <main className='h-screen'>
-      <div className='flex flex-col items-center justify-center font-sans md:h-1/6 pt-4 md:pt-0'>
-        <h1 className='inline-block p-3 pt-0 text-4xl md:text-7xl text-transparent font-bold animate-text bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 bg-clip-text tracking-tight'>
+      <div className='flex flex-col md:items-center justify-center font-sans md:h-1/6 pt-4 md:pt-0 px-4 md:px-0'>
+        <h1 className='inline-block p-3 pt-0 text-3xl md:text-7xl text-transparent font-bold animate-text bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 bg-clip-text tracking-tight'>
           Symposium Events
         </h1>
       </div>
