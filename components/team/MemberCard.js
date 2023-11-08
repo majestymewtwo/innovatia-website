112a9d117568a1bf9a6667a6b17761551ecb9256
Ex-Rockstar <ruthirockstar2218@gmail.com>
2023-11-08 12:29:27 +0530

    member card style change

--- components/team/MemberCard.js
+++ components/team/MemberCard.js
@@ -13,9 +13,9 @@ function MemberCard(props) {
         <div className="img">
           <img src={image} />
         </div>
-        <div className="caption">
-          <h3 className="text-white">{name}</h3>
-          <p className="text-white">{role}</p>
+        <div className="caption text-white">
+          <h3 className="text-bold ">{name}</h3>
+          <p className="text-xs">{role}</p>
           <div className="social-links flex flex-row gap-2 justify-center">
           <a href={github} target="_blank">
             <Github color="#FFD099"/>
