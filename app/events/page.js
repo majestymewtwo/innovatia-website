7a1f51219721f5816a0476a921d869aa75496965
Kamalesh <kamalesh2023@gmail.com>
2023-11-08 15:56:55 +0530

    Social Event

--- app/events/page.js
+++ app/events/page.js
@@ -2,21 +2,22 @@
 import EventTiles from "@/components/events/EventTiles";
 import { useEffect } from "react";
 import { unHideHamButton } from "../utils/utils";
-
+import SocialEvent from "@/components/events/SocialEvent";
 export default function Events() {
   useEffect(() => {
     unHideHamButton();
   }, []);
 
   return (
-    <main className="md:h-screen pt-12 md:pt-0">
+    <main className="md:h-screen h-auto pt-12 md:pt-0">
       <div className="flex flex-col items-center justify-center font-sans md:h-1/6 pt-4 md:pt-0">
         <h1 className="inline-block md:p-3 md:pt-0 text-4xl md:text-7xl text-transparent font-bold animate-text bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 bg-clip-text tracking-tight">
           Symposium Events
         </h1>
       </div>
-      <div className="md:h-5/6 flex justify-center md:p-3">
-        <EventTiles />
+      <div className="md:h-5/6 flex md:flex-row flex-col justify-around md:p-3 w-full">
+        <EventTiles className="w-5/6" />
+        <SocialEvent className="w-1/6" />
       </div>
     </main>
   );
