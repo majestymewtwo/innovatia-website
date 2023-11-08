ccf0033c7b7b425cef49de77a4bd0828a532ccc0
Ex-Rockstar <ruthirockstar2218@gmail.com>
2023-11-08 14:34:05 +0530

    added footer and fixed sidebar transitions

---
+++ components/footer.js
@@ -0,0 +1,7 @@
+export default function Footer(){
+    return (
+        <main className="absolute bottom-0 w-full text-white px-10 py-2">
+            <img src="./sec-logo-white.png" alt="sec-white-logo" className="h-16" />
+        </main>
+    )
+}
