4552dab8777f52d7e113678a9074fd2c3d0130b6
Kamalesh <kamalesh2023@gmail.com>
2023-11-11 12:59:26 +0530

    Non tech event changes

--- components/events/EventTiles.js
+++ components/events/EventTiles.js
@@ -95,50 +95,82 @@ const EventTiles = () => {
 
         <TabsContent
           value="NonTechnical"
-          className="flex md:flex-row flex-col md:m-3 md:-mt-3 md:gap-3 gap-2 m-2 justify-center "
+          className="flex md:flex-row flex-col md:m-3 md:gap-3 justify-center items-center"
         >
-          <Card className="bg-transaprent hover:bg-[url('https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freepik.com%2Ffree-photos-vectors%2Fdesign&psig=AOvVaw3SZmnJcVN7IJReLCVekfO9&ust=1698834866311000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCKjL-JaLoIIDFQAAAAAdAAAAABAE')] text-white md:w-1/3">
-            <Link href="/events/pixel-perfect">
-              <CardHeader>
-                <CardTitle>
-                  <span className="text-sm md:text-2xl">Pixel Perfect</span>
-                </CardTitle>
-                <CardDescription>
-                  Make changes to your Technical here. Click save when you're
-                  done.Make changes to your Technical here. Click save when
-                  you're done.Make changes to your Technical here. Click save
-                  when you're done.
-                </CardDescription>
-              </CardHeader>
-            </Link>
-          </Card>
-          <Card className="bg-transaprent text-white md:w-1/3">
-            <Link href="/events/superhero-shippuden">
-              <CardHeader>
-                <CardTitle>Superhero Shippuden</CardTitle>
-                <CardDescription>
-                  Make changes to your Technical here. Click save when you're
-                  done.Make changes to your Technical here. Click save when
-                  you're done.Make changes to your Technical here. Click save
-                  when you're done.
-                </CardDescription>
-              </CardHeader>
-            </Link>
-          </Card>
-
-          <Card className="bg-transaprent text-white md:w-1/3">
-            <Link href="/events/pop-topia">
-              <CardHeader>
-                <CardTitle>Pop topia</CardTitle>
-                <CardDescription>
-                  Make changes to your Technical here. Click save when you're
-                  done.Make changes to your Technical here. Click save when
-                  you're done.Make changes to your Technical here. Click save
-                  when you're done.
-                </CardDescription>
-              </CardHeader>
-            </Link>
-          </Card>
+          <div class="grid grid-cols-1 gap-3 md:gap-14 md:grid-cols-3 w-4/5 md:w-11/12 -m-3">
+            <div class="group border rounded-lg relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30">
+              <div class="w-full">
+                <img
+                  class="h-full w-full object-cover transition-transform duration-500 group-hover:rotate-3 group-hover:scale-125"
+                  src="/pixelperfect.png"
+                  alt="Pixel-Perfect"
+                />
+              </div>
+              <div class="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70"></div>
+              <div class="absolute inset-0 flex translate-y-[60%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
+                <h1 class="font-dmserif text-3xl font-bold text-white">
+                  Pixel Perfect
+                </h1>
+                <p class="mb-3 text-lg italic text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+                  Photography : ISO, Aperture, Rule of Thirds, Framing Color,
+                  Theory, Camera, DSLR, Mirrorless Cameras
+                </p>
+                <Link href="/events/pixel-perfect">
+                  <button class="rounded-full bg-neutral-900 py-2 px-3.5 font-com text-sm capitalize text-white shadow shadow-black/60 hover:bg-white hover:text-neutral-900">
+                    See More
+                  </button>
+                </Link>
+              </div>
+            </div>
+            <div class="group border rounded-lg relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30">
+              <div class="w-full">
+                <img
+                  class="h-full w-full object-cover transition-transform duration-500 group-hover:rotate-3 group-hover:scale-125"
+                  src="/designderbies.png"
+                  alt="Design-derbies"
+                />
+              </div>
+              <div class="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70"></div>
+              <div class="absolute inset-0 flex translate-y-[60%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
+                <h1 class="font-dmserif text-3xl font-bold text-white">
+                  Superhero Shippuden
+                </h1>
+                <p class="mb-3 text-lg italic text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+                  Anime Trivia: Anime, Manga, Otaku, Cosplay, Shōnen, Shōjo,
+                  Seinen, Josei, Slice of Life
+                </p>
+                <Link href="/events/superhero-shippuden">
+                  <button class="rounded-full bg-neutral-900 py-2 px-3.5 font-com text-sm capitalize text-white shadow shadow-black/60 hover:bg-white hover:text-neutral-900">
+                    See More
+                  </button>
+                </Link>
+              </div>
+            </div>
+            <div class="group border rounded-lg relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30">
+              <div class="w-full">
+                <img
+                  class="h-full w-full object-cover transition-transform duration-500 group-hover:rotate-3 group-hover:scale-125"
+                  src="/designderbies.png"
+                  alt="Design-derbies"
+                />
+              </div>
+              <div class="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70"></div>
+              <div class="absolute inset-0 flex translate-y-[60%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
+                <h1 class="font-dmserif text-3xl font-bold text-white">
+                  Pop Topia
+                </h1>
+                <p class="mb-3 text-lg italic text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+                  Musical Event: Music, Quiz, Trivia, Questionnaire,
+                  Connections, Guessing
+                </p>
+                <Link href="/events/pop-topia">
+                  <button class="rounded-full bg-neutral-900 py-2 px-3.5 font-com text-sm capitalize text-white shadow shadow-black/60 hover:bg-white hover:text-neutral-900">
+                    See More
+                  </button>
+                </Link>
+              </div>
+            </div>
+          </div>
         </TabsContent>
       </Tabs>
     </Card>
