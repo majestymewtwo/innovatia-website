dbc90a67e8c62ab890576b306da7deebc408a8f9
majestymewtwo <majestymewtwo@gmail.com>
2023-11-11 17:06:35 +0530

    home page responsive

--- app/page.js
+++ app/page.js
@@ -53,84 +53,90 @@ export default function Home() {
   return (
     <>
       {/* Desktop VIEW */}
-      <div className="md:block hidden" id="desktop-home">
+      <div className='md:block hidden' id='desktop-home'>
         <main
-          id="app"
-          className="flex py-12 h-screen flex-col items-center justify-between relative overflow-x-hidden"
-        >
-          <div className="flex flex-col md:flex-row w-[80%]">
-            <div className="w-3/4 flex flex-col items-center justify-center ">
+          id='app'
+          className='flex py-12 h-screen flex-col items-center justify-between relative overflow-x-hidden'>
+          <div className='flex flex-col md:flex-row w-[80%]'>
+            <div className='w-3/4 flex flex-col items-center justify-center '>
               <div
-                className="flex flex-col items-center space-y-4"
+                className='flex flex-col items-center space-y-4'
                 onMouseEnter={textEnter}
-                onMouseLeave={textLeave}
-              >
-                <h3 className="text-white text-xl font-blanka font-black tracking-widest">
+                onMouseLeave={textLeave}>
+                <h3 className='text-white text-xl font-blanka font-black tracking-widest'>
                   Department Of M.Tech Computer Science & Engineering
                 </h3>
-                <h3 className="text-white text-xl font-inconsolata">
+                <h3 className='text-white text-xl font-inconsolata'>
                   in association with
                 </h3>
-                <h3 className="text-white text-2xl font-blanka font-black tracking-widest">
+                <h3 className='text-white text-2xl font-blanka font-black tracking-widest'>
                   Sai InfoBlitz
                 </h3>
-                <h3 className="text-white text-xl font-inconsolata">
+                <h3 className='text-white text-xl font-inconsolata'>
                   Proudly Presents
                 </h3>
-                <h3 className="text-4xl md:text-9xl tracking-wider text-transparent font-bold animate-text bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 bg-clip-text font-space py-4">
+                <h3 className='text-4xl md:text-9xl tracking-wider text-transparent font-bold animate-text bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 bg-clip-text font-space py-4'>
                   INNOVATIA
                 </h3>
-                <SpaceButton/>
-                <h3 className="text-white text-2xl font-inconsolata">
+                <SpaceButton />
+                <h3 className='text-white text-2xl font-inconsolata'>
                   Starts In
                 </h3>
-                <CountdownTimer targetDate="2023-11-22T23:59:59" />
+                <CountdownTimer targetDate='2023-11-22T23:59:59' />
               </div>
             </div>
-            <div className="w-1/4 relative">
+            <div className='w-1/4 relative'>
               <Lottie
                 animationData={space1}
-                className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 md:w-[500px]"
+                className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 md:w-[500px]'
               />
             </div>
           </div>
           <motion.div
-            className="custom-cursor"
+            className='custom-cursor'
             variants={variants}
             animate={cursorVariant}
           />
         </main>
       </div>
       {/* MOBILE VIEW */}
-      <div className="block md:hidden">
+      <div className='block md:hidden'>
         <main
-          id="app"
-          className="flex min-h-screen flex-col items-center justify-between relative overflow-x-hidden pt-20"
-        >
-          <div className="flex">
-            <div className="flex items-center justify-center">
-              <div className="flex flex-col items-center justify-center space-y-2">
-                <span className="flex flex-col justify-center items-center">
-                  <h3 className="text-white text-xl font-blanka font-black tracking-wider">
-                    Department Of M.Tech
+          id='app'
+          className='flex min-h-screen flex-col items-center justify-between relative overflow-x-hidden pt-20'>
+          <div className='flex'>
+            <div className='flex items-center justify-center'>
+              <div className='flex flex-col items-center justify-center space-y-4'>
+                <span className='flex flex-col justify-center items-center space-y-4'>
+                  <span className='text-center'>
+                    <h3 className='text-white text-xl font-blanka font-black tracking-wider'>
+                      Department Of M.Tech
+                    </h3>
+                    <h3 className='text-white text-xl font-blanka font-black tracking-wider'>
+                      Computer Science & Engineering
+                    </h3>
+                  </span>
+                  <h3 className='text-white text-lg font-inconsolata'>
+                    in association with
                   </h3>
-                  <h3 className="text-white text-xl font-blanka font-black tracking-wider">
-                    Computer Science & Engineering
+                  <h3 className='text-white text-2xl font-blanka font-black tracking-widest'>
+                    Sai InfoBlitz
                   </h3>
                 </span>
-                <h3 className="text-white text-xl font-inconsolata">
+                <h3 className='text-white text-lg font-inconsolata'>
                   Proudly Presents
                 </h3>
-                <h3 className="text-4xl md:text-9xl tracking-wider text-transparent font-bold animate-text bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 bg-clip-text font-space py-4">
+                <h3 className='text-6xl tracking-wider text-transparent font-bold animate-text bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 bg-clip-text font-space py-4'>
                   INNOVATIA
                 </h3>
-                <h3 className="text-white text-2xl font-inconsolata">
+                <h3 className='text-white text-2xl font-inconsolata'>
                   Starts In
                 </h3>
-                <CountdownTimer targetDate="2023-11-22T23:59:59" />
-                <div className="w-1/2">
+                <CountdownTimer targetDate='2023-11-22T23:59:59' />
+                <div className='w-1/2'>
                   <Lottie animationData={space1} />
                 </div>
+                <SpaceButton />
               </div>
             </div>
           </div>
