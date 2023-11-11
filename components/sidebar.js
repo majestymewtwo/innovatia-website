b751b1481b20eb9b19a00864a768458345d4f3e7
majestymewtwo <majestymewtwo@gmail.com>
2023-11-11 17:03:08 +0530

    team page styling issue fix

--- components/sidebar.js
+++ components/sidebar.js
@@ -28,42 +28,38 @@ function Sidebar({ handleCallback }) {
       <div
         className={`fixed right-0 p-4 z-10`}
         onClick={toggleMenu}
-        id="ham-btn"
-      >
-        <div className={`button${active ? " active" : ""}`} id="btn">
-          <div className="bar top"></div>
-          <div className="bar middle"></div>
-          <div className="bar bottom"></div>
+        id='ham-btn'>
+        <div className={`button${active ? " active" : ""}`} id='btn'>
+          <div className='bar top'></div>
+          <div className='bar middle'></div>
+          <div className='bar bottom'></div>
         </div>
       </div>
       <div className={`sidebar ${active && "active bg-white bg-opacity-5"}`}>
-        <ul className="space-y-14 sidebar-list font-space text-xl">
+        <ul className='space-y-14 sidebar-list font-space text-xl'>
           <li className={`sidebar-item ${active && "active"}`}>
-            <Link href="/" className="sidebar-anchor" onClick={toggleMenu}>
+            <Link href='/' className='sidebar-anchor' onClick={toggleMenu}>
               Home
             </Link>
           </li>
           <li
             className={`sidebar-item ${active && "active"}`}
-            onClick={toggleMenu}
-          >
-            <Link href="/about" className="sidebar-anchor">
+            onClick={toggleMenu}>
+            <Link href='/about' className='sidebar-anchor'>
               About
             </Link>
           </li>
           <li
             className={`sidebar-item ${active && "active"}`}
-            onClick={toggleMenu}
-          >
-            <Link href="/team" className="sidebar-anchor">
+            onClick={toggleMenu}>
+            <a href='/team' className='sidebar-anchor'>
               Our Team
-            </Link>
+            </a>
           </li>
           <li
             className={`sidebar-item ${active && "active"}`}
-            onClick={toggleMenu}
-          >
-            <Link href="/events" className="sidebar-anchor">
+            onClick={toggleMenu}>
+            <Link href='/events' className='sidebar-anchor'>
               Events
             </Link>
           </li>
