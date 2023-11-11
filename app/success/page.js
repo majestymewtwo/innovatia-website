5aa31626475242ade43b7dab0a33ff462ff79bf3
majestymewtwo <majestymewtwo@gmail.com>
2023-11-11 16:43:39 +0530

    succes page redirection complete

--- app/success/page.js
+++ app/success/page.js
@@ -2,11 +2,27 @@
 import { useEffect, useState } from "react";
 import Confetti from "@/components/register/Confetti";
 import { unHideHamButton } from "../utils/utils";
+import { Card } from "@/components/ui/card";
 
-const Success = (props) => {
+const Success = ({ searchParams }) => {
   const [visible, setVisible] = useState(true);
-  const serializedUser = props.searchParams.user;
-  const user = JSON.parse(serializedUser);
+  const data = JSON.parse(searchParams.data);
+
+  const titleClass = "flex items-center space-x-4 text-lg md:text-xl";
+  const headingClass = "font-bold";
+  const detailClass = "font-light";
+
+  const renderMemberDetails = (label, value) => {
+    if (value !== null && value !== undefined) {
+      return (
+        <div className={titleClass}>
+          <h1 className={headingClass}>{label}</h1>
+          <h1 className={detailClass}>{value}</h1>
+        </div>
+      );
+    }
+    return null;
+  };
 
   useEffect(() => {
     unHideHamButton();
@@ -14,9 +30,55 @@ const Success = (props) => {
 
   return (
     <>
-      <div className='flex justify-center w-full h-screen pt-6'>
-        {user.name + " " + user.email}
+      <div className='flex flex-col space-y-6 items-center justify-center w-full h-screen pt-6'>
+        <h2 className='inline-block md:p-3 md:pt-0 text-3xl md:text-5xl text-transparent font-bold animate-text bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 bg-clip-text tracking-tight'>
+          Registration Successful
+        </h2>
+
+        <Card className='grid grid-cols-2 gap-3 bg-white/5 text-white w-[80%] p-3'>
+          {renderMemberDetails("Team Name", data.teamName)}
+
+          {/* Member 1 */}
+          {renderMemberDetails("Member 1 Name", data.memberOneName)}
+          {renderMemberDetails("Member 1 College ID", data.memberOneCollegeId)}
+          {renderMemberDetails("Member 1 Mail ID", data.memberOneMailId)}
+          {renderMemberDetails("Member 1 Phone", data.memberOnePhoneNo)}
+          {renderMemberDetails("Member 1 Department", data.memberOneDept)}
+          {renderMemberDetails("Member 1 Year", data.memberOneYear)}
+          {renderMemberDetails(
+            "Member 1 College Name",
+            data.memberOneCollegeName
+          )}
+
+          {/* Member 2 */}
+          {renderMemberDetails("Member 2 Name", data.memberTwoName)}
+          {renderMemberDetails("Member 2 College ID", data.memberTwoCollegeId)}
+          {renderMemberDetails("Member 2 Mail ID", data.memberTwoMailId)}
+          {renderMemberDetails("Member 2 Phone", data.memberTwoPhoneNo)}
+          {renderMemberDetails("Member 2 Department", data.memberTwoDept)}
+          {renderMemberDetails("Member 2 Year", data.memberTwoYear)}
+          {renderMemberDetails(
+            "Member 2 College Name",
+            data.memberTwoCollegeName
+          )}
+
+          {/* Member 3 */}
+          {renderMemberDetails("Member 3 Name", data.memberThreeName)}
+          {renderMemberDetails(
+            "Member 3 College ID",
+            data.memberThreeCollegeId
+          )}
+          {renderMemberDetails("Member 3 Mail ID", data.memberThreeMailId)}
+          {renderMemberDetails("Member 3 Phone", data.memberThreePhoneNo)}
+          {renderMemberDetails("Member 3 Department", data.memberThreeDept)}
+          {renderMemberDetails("Member 3 Year", data.memberThreeYear)}
+          {renderMemberDetails(
+            "Member 3 College Name",
+            data.memberThreeCollegeName
+          )}
+        </Card>
       </div>
+
       {visible && <Confetti />}
     </>
   );
