13020f9f51e6743e5a460daefd6ac6055630bcd4
Ex-Rockstar <ruthirockstar2218@gmail.com>
2023-11-12 16:51:54 +0530

    Created Contact Us page

---
+++ app/contact/page.js
@@ -0,0 +1,12 @@
+import ContactUs from '@/components/contact/ContactUs'
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+      <ContactUs/>
+    </div>
+  )
+}
+
+export default page
---
+++ components/contact/ContactUs.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ContactUs() {
+  return (
+    <div className='flex justify-center items-center'>
+        Coming Soon!!!
+    </div>
+  )
+}
+
+export default ContactUs
