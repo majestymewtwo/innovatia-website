4481bc128626f32d5550d92c2c48d707714888dd
Ex-Rockstar <ruthirockstar2218@gmail.com>
2023-11-12 21:56:05 +0530

    Superhero Shippuden Info added

--- app/events/superhero-shippuden/page.js
+++ app/events/superhero-shippuden/page.js
@@ -1,13 +1,13 @@
-'use client'
+"use client";
 
 import { Card } from "@/components/ui/card";
-import React, { use, useEffect } from "react";
+import React, { useEffect } from "react";
 import { Button } from "@/components/ui/button";
 import "../eventdetail.css";
 import { usePathname, useRouter } from "next/navigation";
 import { unHideHamButton } from "@/app/utils/utils";
 
-const EventDetail = () => {
+const EventDetailDB = () => {
   const onDownload = () => {
     const link = document.createElement("a");
     link.download = `space-bg-1.jpg`;
@@ -24,100 +24,173 @@ const EventDetail = () => {
   }, []);
 
   return (
-    <section className='h-[100vh] w-[100vw] flex justify-center items-center'>
-      <div className='flex h-[75%] w-[90%] md:h-[83%] lg:h-[80%] lg:justify-center'>
-        <Card className='relative w-[95%] bg-white/5 flex flex-col backdrop-blur-sm items-center h-full p-4 md:h-full lg:flex-row lg:items-start lg:pt-16 text-white space-y-4 lg:space-y-0 overflow-clip'>
-          <div className='flex w-full items-center justify-between flex-row-reverse h-fit md:w-1/2 lg:w-1/2 lg:h-4/5 lg:pr-6 lg:justify-center'>
+    <section className="h-[100vh] w-[100vw] flex justify-center items-center">
+      <div className="flex h-[75%] w-[90%] md:h-[83%] lg:h-[80%] lg:justify-center">
+        <Card className="relative w-[95%] bg-white/5 flex flex-col backdrop-blur-sm items-center h-full p-4 md:h-full lg:flex-row lg:items-start lg:pt-16 text-white space-y-4 lg:space-y-0 overflow-clip">
+          <div className="flex w-full items-center justify-between flex-row-reverse h-fit md:w-1/2 lg:w-1/2 lg:h-4/5 lg:pr-6 lg:justify-center">
             <img
-              src='/retro-bg.jpg'
-              alt='retro-bg'
-              className='rounded-md w-[150px] h-full md:w-[200px] md:h-[200px] lg:w-auto lg:h-full lg:rounded-md'
+              src="/retro-bg.jpg"
+              alt="retro-bg"
+              className="rounded-md w-[150px] h-full md:w-[200px] md:h-[200px] lg:w-auto lg:h-full lg:rounded-md"
             />
-            <div className='font-oswald text-4xl items-center text-[#EF8354] md:text-4xl lg:hidden'>
+            <div className="font-oswald text-4xl items-center text-[#EF8354] md:text-4xl lg:hidden">
               <p>Superhero</p>
               <p>Shippuden</p>
             </div>
           </div>
-          <div className='flex flex-col space-y-3 h-3/5 lg:w-1/2 lg:h-4/5  lg:items-left overflow-y-clip'>
-            <div className='hidden font-oswald text-2xl text-left text-[#EF8354] lg:text-4xl lg:block'>
-              <p>Superhero Shippuden</p>
+          <div className="flex flex-col space-y-3 h-3/5 lg:w-1/2 lg:h-4/5  lg:items-left overflow-y-clip">
+            <div className="hidden font-oswald text-2xl text-left text-[#EF8354] lg:text-4xl lg:block">
+              <p>SUPERHERO SHIPPUDEN</p>
             </div>
-            <div className='pr-2 space-y-2 overflow-y-scroll scrollbar lg:text-sm'>
-              <p className='font-semibold'>
-                Unique, United and Unyielding. Compose your designs with bags of
-                characters to make the trophy your winning glory.
+            <div className="pr-2 space-y-2 overflow-y-scroll scrollbar lg:text-sm">
+              <p className="font-semibold">
+                "Unleash Your Superpower Fusion" To celebrate and test the
+                knowledge of diverse superhero and anime fandoms while fostering
+                teamwork and creativity.
               </p>
               <p>
-                <span className='font-bold'>Mode: </span>Both Online and Offline
+                <span className="font-bold">Mode: </span>
+                <ul>
+                  <li>1st Round: Online </li>
+                  <li>2nd Round: Offline </li>
+                </ul>
               </p>
               <p>
-                <span className='font-bold'>Date and Time: </span>22 nd November
-                - 9:30 AM
+                <span className="font-bold">Date and Time: </span>
+                <ul>
+                  <li>18th November 2023 (Online)</li>
+                  <li>22nd November 2023 (Offline)</li>
+                </ul>
               </p>
-              <span className='font-bold'>Instructions: </span>
-              <ul className='list-disc list-inside'>
-                <li>Allowed tools: Figma, Adobe XD.</li>
+              <p>
+                <span className="font-bold">Venue : </span>
+                <ul>
+                  <li>
+                    Venue: Sri Sairam Engineering College, Chennai. (Gamma hall
+                    - Superhero shippuden)
+                  </li>
+                </ul>
+              </p>
+              <p>
+                <span className="font-bold">Team : </span>
+                <ul>
+                  <li>Group Of 2 or 3</li>
+                </ul>
+              </p>
+              <p>
+                <span className="font-bold">Reporting Time: </span>
+                <ul>
+                  <li>9:15 AM</li>
+                </ul>
+              </p>
+              <p>
+                <span className="font-bold">Event Time : </span>
+                <ul>
+                  <li>11:15 AM</li>
+                </ul>
+              </p>
+              <span className="font-bold">Instructions: </span>
+              <ul className="list-disc list-inside">
                 <li>
-                  Duration- 2 days (Round 1- Online, Round 2 & 3 - Offline).
+                  <span className="font-bold">ONLINE QUIZ</span> <br />
+                  <div className="ml-5">
+                    <span className="font-bold"> Free for All Quiz</span> <br />
+                    1. Mixed Questions: Prepare a mix of questions from comics,
+                    anime, and series. <br />
+                    2. Pass Option: If a team can't answer, the question passes
+                    to the next team.
+                    <br />
+                    <span className="font-bold">
+                      Scene Identification Challenge
+                    </span>{" "}
+                    <br />
+                    1. Number Selection: Teams pick numbers within a specific
+                    range.
+                    <br />
+                    2. Video-Based Questions: Videos play scenes in ¼ screen.
+                    <br />
+                    3. Goal: Teams identify the character and series based on
+                    the scene
+                    <br />
+                    4. Top teams will be shortlisted to the offline rounds to be
+                    held on 22nd
+                  </div>
                 </li>
-                <li> Team should consist of 2 members.</li>
                 <li>
-                  Duration- 2 days (Round 1- Online, Round 2 & 3 - Offline).
+                  <span className="font-bold">Round 1:</span> <br />
+                  <div className="ml-5">
+                    <span className="font-bold">Series Name Guessing</span>{" "}
+                    <br />
+                    1. Team member silently gestures series/movie name.
+                    <br />
+                    2. Others guess while listening to loud music on headphones.
+                    <br />
+                    3. Teams will be awarded points based on their guesses
+                  </div>
                 </li>
-                <li> Team should consist of 2 members.</li>
                 <li>
-                  Duration- 2 days (Round 1- Online, Round 2 & 3 - Offline).
+                  <span className="font-bold">Round 2:</span> <br />
+                  <div className="ml-5">
+                    <span className="font-bold">Chuunin Exams Event</span>
+                    <br />
+                    1. Test Format: Pen and paper MCQ test.
+                    <br />
+                    2. Topics: Series and anime knowledge assessment.
+                  </div>
                 </li>
-                <li> Team should consist of 2 members.</li>
+                <li>
+                  <span className="font-bold">Round 3:</span> <br />
+                  <div className="ml-5">
+                    <span className="font-bold">Shipwreck Scenario</span>
+                    <br />
+                    1. Hypothetical Situation: Ship sinking, one life jacket
+                    available. <br />
+                    2. Character Impersonation: Participants embody characters
+                    (anime, Marvel, DC).
+                    <br />
+                    3. Three Rounds: Shortlisting rounds and a final showdown.
+                    <br />
+                  </div>
+                </li>
+                <li>
+                  WINNER : Team with the highest overall score wins the
+                  competition.
+                </li>
               </ul>
               <p>
-                <span className='font-bold'>Venue: </span>Gamma Hall
+                <span className="font-bold">Venue: </span>Gamma Hall
               </p>
-              <p className='font-bold'>
-                Round 1-(Duration: 1.5 hours, Online on Discord)
+              <p className="font-bold">
+                Last Date for Registration: 18.11.2023(11:00 AM)
               </p>
-              <ul className='list-disc list-inside'>
-                <li>
-                  Participants will be given distinct problem statements to
-                  design Three pages of a web applications.
-                </li>
-                <li>
-                  The registered teams will further be grouped together, top 3
-                  teams from each group will be selected to Round 2.
-                </li>
-              </ul>
-              <p className='font-bold'>
-                Round 2-(Duration: 45 minutes, Offline)
+              <p>
+                <span className="font-bold">Whatsapp Group Link : </span>{" "}
+                <a href="" target="_blank">
+                  Join Superhero Shippuden!
+                </a>
               </p>
-              <ul className='list-disc list-inside'>
-                <li>
-                  Teams moved to this round will be given a problem statement to
-                  design a Logo.
-                </li>
-                <li>
-                  {" "}
-                  If needed both the team members can work simultaneously on
-                  their respective Laptops.
-                </li>
-              </ul>
             </div>
           </div>
-          <div className='flex flex-col w-full justify-start lg:w-auto lg:absolute lg:bottom-10 lg:left-5'>
-            <p className='text-sm font-semibold'>Contact</p>
-            <p className='text-sm'>+91 87545 12892 - Yogesh M</p>
-            <p className='text-sm'>+91 97890 27587 - Muthu Aanand S U</p>
+          <div className="flex flex-col w-full justify-start lg:w-auto lg:absolute lg:bottom-5 lg:left-5">
+            <p className="text-sm font-semibold">Student Coordinator</p>
+            <p className="text-sm">+91 9003268427 - Guru Sanjay R K</p>
+            <p className="text-sm">+91 9486308851 - Ruthi Shankari S</p>
+            <p className="text-sm font-semibold">Staff Coordinator</p>
+            <p className="text-sm">+91 99622 75202 - Mrs.SHINY A</p>
           </div>
-          <div className='flex flex-row justify-center w-full space-x-5 lg:w-auto lg:absolute lg:bottom-10 lg:left-1/2'>
+          <div className="flex flex-row justify-center w-full space-x-5 lg:w-auto lg:absolute lg:bottom-10 lg:left-1/2">
             <Button
-              variant='contained'
-              className='border w-1/2 border-gray-300 hover:bg-[#E97451] text-sm '
-              onClick={() => router.push(`${pathName}/register`)}>
+              variant="contained"
+              className="border w-1/2 border-gray-300 hover:bg-[#E97451] text-sm "
+              onClick={() => router.push(`${pathName}/register`)}
+            >
               Register Now
             </Button>
             <Button
-              variant='contained'
-              className='border w-1/2 p-4 border-gray-300 hover:bg-[#E97451]  '
-              onClick={onDownload}>
+              variant="contained"
+              className="border w-1/2 p-4 border-gray-300 hover:bg-[#E97451]  "
+              onClick={onDownload}
+            >
               View Brochure
             </Button>
           </div>
@@ -127,4 +200,4 @@ const EventDetail = () => {
   );
 };
 
-export default EventDetail
+export default EventDetailDB;
