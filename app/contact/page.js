915ce0ce3855e4164fd69fb141ed97832d3d950f
majestymewtwo <majestymewtwo@gmail.com>
2023-11-13 08:14:30 +0530

    contact us page style

--- app/contact/page.js
+++ app/contact/page.js
@@ -1,12 +1,18 @@
-import ContactUs from '@/components/contact/ContactUs'
-import React from 'react'
+"use client";
+import ContactUs from "@/components/contact/ContactUs";
+import React, { useEffect } from "react";
+import { unHideHamButton } from "../utils/utils";
 
 function page() {
+  useEffect(() => {
+    unHideHamButton();
+  }, []);
+
   return (
-    <div>
-      <ContactUs/>
+    <div className='flex justify-center items-center text-center min-h-[85vh]'>
+      <ContactUs />
     </div>
-  )
+  );
 }
 
-export default page
+export default page;
--- components/contact/ContactUs.js
+++ components/contact/ContactUs.js
@@ -2,10 +2,12 @@ import React from 'react'
 
 function ContactUs() {
   return (
-    <div className='flex justify-center items-center'>
-        Coming Soon!!!
+    <div>
+      <h1 className='font-blanka text-7xl bg-gradient-to-br from-pink-500 via-red-500 to-yellow-500 bg-clip-text text-transparent p-2 animate-text'>
+        Coming Soon
+      </h1>
     </div>
-  )
+  );
 }
 
 export default ContactUs
