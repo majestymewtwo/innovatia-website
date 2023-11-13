0fa16d9dad3e6815574c0ea3552807c2e85b2e6e
Guru Sanjay <gurucoolmsd@gmail.com>
2023-11-13 12:13:17 +0530

    fxed hamburger menu in schedule

--- app/schedule/page.js
+++ app/schedule/page.js
@@ -1,12 +1,17 @@
-import Schedule from '@/components/schedule/Schedule'
-import React from 'react'
-
+'use client'
+import Schedule from "@/components/schedule/Schedule";
+import { React, useEffect } from "react";
+import { unHideHamButton } from "../utils/utils";
 function page() {
+  useEffect(() => {
+    unHideHamButton();
+  }, []);
+
   return (
-    <div className='flex justify-center items-center text-center min-h-[85vh]'>
-      <Schedule/>
+    <div className="flex justify-center items-center text-center min-h-[85vh]">
+      <Schedule />
     </div>
-  )
+  );
 }
 
-export default page
+export default page;
