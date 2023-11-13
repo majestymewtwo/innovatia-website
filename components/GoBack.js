8b83c2ad2f0cd2f5f1d0662c75d8f24636ebe37c
Guru Sanjay <gurucoolmsd@gmail.com>
2023-11-13 12:09:52 +0530

    Added a tentative goback button

--- components/GoBack.js
+++ components/GoBack.js
@@ -2,13 +2,14 @@ import Lottie from 'lottie-react'
 import React from 'react'
 import data from '../public/space-12.json'
 import { useRouter } from 'next/navigation'
+// import "../public/goback.png"
 function GoBack() {
   const router = useRouter()
   return (
-    <div>
+    <div className='m-10 transition ease-in-out delay-150  hover:scale-125   '>
      
       <button onClick={()=>router.back()}>
-        <div className='w-[100px] h-[100px] transform -rotate-90 '><Lottie animationData={data}/></div></button>
+        <img src="/goback1.png" alt="" width={60} height={60} /></button>
     </div>
   )
 }
