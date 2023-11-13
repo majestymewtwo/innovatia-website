864f55b755aec097a8011e663f01aca077f255b2
majestymewtwo <majestymewtwo@gmail.com>
2023-11-13 20:04:24 +0530

    initial seo configuration

--- app/layout.js
+++ app/layout.js
@@ -3,52 +3,52 @@ import "./globals.css";
 import Sidebar from "@/components/sidebar";
 import { Toaster } from "@/components/ui/toaster";
 import Footer from "@/components/footer";
+import { NextSeo } from "next-seo";
+import { NEXT_SEO_DEFAULT } from "@/next-seo.config";
 
 const inter = Inter({ subsets: ["latin"] });
 const lucky = Luckiest_Guy({ subsets: ["latin"], weight: "400" });
 const quick = Quicksand({ subsets: ["latin"] });
 
-export const metadata = {
-  title:
-    "Innovatia 2.0 | Department Of M.Tech Computer Science And Engineering | Sri Sai Ram Engineering College | Chennai",
-  description:
-    "Join us at the M.Tech CSE Department Symposium — a day of tech innovation and creative exploration. Elevate your skills, connect with peers, and immerse yourself in a blend of technical and non-technical events. Unleash your potential and be part of the future of technology at Sri Sai Ram Engineering College.",
-};
+// export const metadata = {
+//   title:
+//     "Innovatia 2.0 | Department Of M.Tech Computer Science And Engineering | Sri Sai Ram Engineering College | Chennai",
+//   description:
+//     "Join us at the M.Tech CSE Department Symposium — a day of tech innovation and creative exploration. Elevate your skills, connect with peers, and immerse yourself in a blend of technical and non-technical events. Unleash your potential and be part of the future of technology at Sri Sai Ram Engineering College.",
+// };
 
 //test commit
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <head>
+        <NextSeo useAppDir={true} {...NEXT_SEO_DEFAULT} />
         <link
-          rel="shortcut icon"
-          href="./rocket-cursor.png"
-          type="image/x-icon"
+          rel='shortcut icon'
+          href='./rocket-cursor.png'
+          type='image/x-icon'
         />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel='preconnect' href='https://fonts.googleapis.com' />
         <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="true"
+          rel='preconnect'
+          href='https://fonts.gstatic.com'
+          crossOrigin='true'
         />
         <link
-          href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600;700&display=swap"
-          rel="stylesheet"
-        ></link>
+          href='https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600;700&display=swap'
+          rel='stylesheet'></link>
         <link
-          href="https://fonts.googleapis.com/css2?family=Source+Serif+4:ital,opsz,wght@0,8..60,200;0,8..60,300;0,8..60,400;0,8..60,500;0,8..60,600;0,8..60,700;0,8..60,800;0,8..60,900;1,8..60,200;1,8..60,300;1,8..60,400;1,8..60,500;1,8..60,600;1,8..60,700;1,8..60,800;1,8..60,900&display=swap"
-          rel="stylesheet"
-        ></link>
+          href='https://fonts.googleapis.com/css2?family=Source+Serif+4:ital,opsz,wght@0,8..60,200;0,8..60,300;0,8..60,400;0,8..60,500;0,8..60,600;0,8..60,700;0,8..60,800;0,8..60,900;1,8..60,200;1,8..60,300;1,8..60,400;1,8..60,500;1,8..60,600;1,8..60,700;1,8..60,800;1,8..60,900&display=swap'
+          rel='stylesheet'></link>
         <link
-          href="https://fonts.googleapis.com/css2?family=Inconsolata:wght@200;300;400;500;600;700;800;900&display=swap"
-          rel="stylesheet"
-        ></link>
-        <link href="https://fonts.cdnfonts.com/css/blanka" rel="stylesheet" />
+          href='https://fonts.googleapis.com/css2?family=Inconsolata:wght@200;300;400;500;600;700;800;900&display=swap'
+          rel='stylesheet'></link>
+        <link href='https://fonts.cdnfonts.com/css/blanka' rel='stylesheet' />
       </head>
       <body>
         <Sidebar />
-        <main id="app" className="h-[100vh] relative overflow-y-scroll">
+        <main id='app' className='h-[100vh] relative overflow-y-scroll'>
           {children}
           <Footer />
         </main>
---
+++ next-seo.config.js
@@ -0,0 +1,16 @@
+export const NEXT_SEO_DEFAULT = {
+  title:
+    "Innovatia 2.0 | Department Of M.Tech Computer Science And Engineering | Sri Sai Ram Engineering College | Chennai",
+  description:
+    "Join us at the M.Tech CSE Department Symposium — a day of tech innovation and creative exploration. Elevate your skills, connect with peers, and immerse yourself in a blend of technical and non-technical events. Unleash your potential and be part of the future of technology at Sri Sai Ram Engineering College.",
+  openGraph: {
+    type: "website",
+    locale: "en_IE",
+    url: "https://www.innovatia23.tech",
+    title:
+      "Innovatia 2.0 | Department Of M.Tech Computer Science And Engineering | Sri Sai Ram Engineering College | Chennai",
+    description:
+      "Join us at the M.Tech CSE Department Symposium — a day of tech innovation and creative exploration. Elevate your skills, connect with peers, and immerse yourself in a blend of technical and non-technical events. Unleash your potential and be part of the future of technology at Sri Sai Ram Engineering College.",
+    siteName: "Innovatia 2.0 Symposium",
+  },
+};
