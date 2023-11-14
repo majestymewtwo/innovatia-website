0c0b4e1a68f51ec8855bb21c94c7abce26393d6f
majestymewtwo <majestymewtwo@gmail.com>
2023-11-14 08:17:09 +0530

    success page styles and goback button added

--- components/GoBack.js
+++ components/GoBack.js
@@ -2,12 +2,18 @@ import Lottie from 'lottie-react'
 import React from 'react'
 import data from '../public/space-12.json'
 import { useRouter } from 'next/navigation'
-// import "../public/goback.png"
-function GoBack() {
-  const router = useRouter()
+
+function GoBack({ path }) {
+  const router = useRouter();
+
+  const navigate = () => {
+    if (path) router.push(path);
+    else router.back();
+  };
+
   return (
     <div className='absolute top-0 left-0 p-5 md:p-4'>
-      <button onClick={() => router.back()}>
+      <button onClick={navigate}>
         <img src='/go-back.png' alt='' width={50} height={50} />
       </button>
     </div>
