b58263dc75610cbb886aa87ec01eff30633e735d
Kamalesh <kamalesh2023@gmail.com>
2023-11-16 15:47:41 +0530

    Home fix

--- app/page.js
+++ app/page.js
@@ -60,93 +60,96 @@ export default function Home() {
   if (suspense) return <Loading />;
 
   return (
-    <div className='space-y-8'>
+    <div className="space-y-8">
       {/* Desktop VIEW */}
-      <div className='hidden md:block' id='desktop-home'>
+      <div className="hidden md:block" id="desktop-home">
         <main
-          id='app'
-          className='relative flex flex-col items-center justify-between min-h-screen py-12 overflow-x-hidden'>
-          <div className='flex flex-col md:flex-row w-[80%]'>
-            <div className='flex flex-col items-center justify-center w-3/4 '>
+          id="app"
+          className="relative flex flex-col items-center justify-between min-h-screen py-12 overflow-x-hidden"
+        >
+          <div className="flex flex-col md:flex-row w-[80%]">
+            <div className="flex flex-col items-center justify-center w-3/4 ">
               <div
-                className='flex flex-col items-center space-y-4'
+                className="flex flex-col items-center space-y-4"
                 onMouseEnter={textEnter}
-                onMouseLeave={textLeave}>
-                <h3 className='text-xl font-black tracking-widest text-white font-blanka'>
+                onMouseLeave={textLeave}
+              >
+                <h3 className="text-xl font-black tracking-widest text-white font-blanka">
                   Department Of M.Tech Computer Science & Engineering
                 </h3>
-                <h3 className='text-xl text-white font-inconsolata'>
+                <h3 className="text-xl text-white font-inconsolata">
                   in association with
                 </h3>
-                <h3 className='text-2xl font-black tracking-widest text-white font-blanka'>
+                <h3 className="text-2xl font-black tracking-widest text-white font-blanka">
                   Sai InfoBlitz
                 </h3>
-                <h3 className='text-xl text-white font-inconsolata'>
+                <h3 className="text-xl text-white font-inconsolata">
                   Proudly Presents
                 </h3>
-                <h3 className='text-4xl md:text-9xl tracking-wider text-transparent font-bold animate-text bg-gradient-to-r from-[#0231DD] via-[#6681db] to-[#08A8A7] bg-clip-text font-space py-4'>
+                <h3 className="text-4xl md:text-9xl tracking-wider text-transparent font-bold animate-text bg-gradient-to-r from-[#0231DD] via-[#6681db] to-[#08A8A7] bg-clip-text font-space py-4">
                   INNOVATIA
                 </h3>
                 <SpaceButton />
-                <h3 className='text-2xl text-white font-inconsolata'>
+                <h3 className="text-2xl text-white font-inconsolata">
                   Starts In
                 </h3>
-                <CountdownTimer targetDate='2023-11-22T23:59:59' />
-                <h3 className='text-2xl text-white font-inconsolata'>
-                  <span className='font-bold'>Venue : </span> MBA Auditorium (D
+                <CountdownTimer targetDate="2023-11-22T23:59:59" />
+                <h3 className="text-2xl text-white font-inconsolata">
+                  <span className="font-bold">Venue : </span> MBA Auditorium (D
                   Block,III Floor)
                 </h3>
               </div>
             </div>
-            <div className='relative w-1/4'>
+            <div className="relative w-1/4">
               <Lottie
                 animationData={space1}
-                className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 md:w-[500px]'
+                className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 md:w-[500px]"
               />
             </div>
           </div>
           <motion.div
-            className='custom-cursor'
+            className="custom-cursor"
             variants={variants}
             animate={cursorVariant}
           />
         </main>
       </div>
       {/* MOBILE VIEW */}
-      <div className='block md:hidden'>
+      <div className="block md:hidden">
         <main
-          id='app'
-          className='relative flex flex-col items-center justify-between min-h-screen pt-20 overflow-x-hidden'>
-          <div className='flex'>
-            <div className='flex items-center justify-center'>
-              <div className='flex flex-col items-center justify-center space-y-2'>
-                <span className='flex flex-col items-center justify-center space-y-3'>
-                  <span className='text-center'>
-                    <h3 className='text-xl font-black tracking-wider text-white font-blanka'>
+          id="app"
+          className="relative flex flex-col items-center justify-between min-h-screen pt-20 overflow-x-hidden"
+        >
+          <div className="flex">
+            <div className="flex items-center justify-center">
+              <div className="flex flex-col items-center justify-center space-y-2">
+                <span className="flex flex-col items-center justify-center space-y-3">
+                  <span className="text-center">
+                    <h3 className="text-xl font-black tracking-wider text-white font-blanka">
                       Department Of M.Tech
                     </h3>
-                    <h3 className='text-xl font-black tracking-wider text-white font-blanka'>
+                    <h3 className="text-xl font-black tracking-wider text-white font-blanka">
                       Computer Science & Engineering
                     </h3>
                   </span>
-                  <h3 className='text-lg text-white font-inconsolata'>
+                  <h3 className="text-lg text-white font-inconsolata">
                     in association with
                   </h3>
-                  <h3 className='text-2xl font-black tracking-widest text-white font-blanka'>
+                  <h3 className="text-2xl font-black tracking-widest text-white font-blanka">
                     Sai InfoBlitz
                   </h3>
                 </span>
-                <h3 className='text-lg text-white font-inconsolata'>
+                <h3 className="text-lg text-white font-inconsolata">
                   Proudly Presents
                 </h3>
-                <h3 className='text-6xl tracking-wider text-transparent font-bold animate-text bg-gradient-to-r from-[#0231DD] via-[#6681db] to-[#08A8A7] bg-clip-text font-space py-4'>
+                <h3 className="text-6xl tracking-wider text-transparent font-bold animate-text bg-gradient-to-r from-[#0231DD] via-[#6681db] to-[#08A8A7] bg-clip-text font-space py-4">
                   INNOVATIA
                 </h3>
-                <h3 className='text-2xl text-white font-inconsolata'>
+                <h3 className="text-2xl text-white font-inconsolata">
                   Starts In
                 </h3>
-                <CountdownTimer targetDate='2023-11-22T23:59:59' />
-                <div className='w-1/2'>
+                <CountdownTimer targetDate="2023-11-22T23:59:59" />
+                <div className="w-1/2">
                   <Lottie animationData={space1} />
                 </div>
                 <SpaceButton />
@@ -156,20 +159,24 @@ export default function Home() {
         </main>
       </div>
       <HeartHandshake
-        className='absolute w-10 h-10 cursor-pointer bottom-4 right-4 md:h-16 md:w-16 text-emerald-500 md:bottom-10 md:right-10 animate-bounce'
+        className="absolute w-10 h-10 cursor-pointer bottom-4 right-4 md:h-16 md:w-16 text-emerald-500 md:bottom-10 md:right-10 animate-bounce"
         onClick={handleDonateClick}
       />
       <Guest
-        image='/muthulakshmi.jpg'
-        name='Muthu Lakshmi'
-        quote='Lakshmi, Entrepreneur-in-Residence at TCS, leverages 20 years of leadership experience, notably as Head of Industry Solutions Practice in Google Business Unit. Passionate about automation and AI, she incubates innovative ideas into new business opportunities. Lakshmi holds a Bachelor of Engineering degree and has completed the Senior Management Program at the Indian Institute of Management, Ahmedabad.'
-        role='Entrepreneur in Residence, Product Leader, TCS'
+        image="/muthulakshmi.jpg"
+        name="Muthu Lakshmi"
+        quote="Lakshmi, Entrepreneur-in-Residence at TCS, leverages 20 years of leadership experience, notably as Head of Industry Solutions Practice in Google Business Unit. Passionate about automation and AI, she incubates innovative ideas into new business opportunities. Lakshmi holds a Bachelor of Engineering degree and has completed the Senior Management Program at the Indian Institute of Management, Ahmedabad."
+        role="Entrepreneur in Residence, Product Leader, TCS"
       />
       <div
-        id='donation'
+        id="donation"
         ref={donationRef}
-        className='flex flex-col items-center justify-around md:flex-row'>
-        <Lottie animationData={donation2LottieJson} className='md:w-1/3' />
+        className="flex flex-col-reverse items-center justify-around md:flex-row"
+      >
+        <Lottie
+          animationData={donation2LottieJson}
+          className="md:w-1/3 w-1/2"
+        />
         <DonationStall />
       </div>
     </div>
--- components/Guest.js
+++ components/Guest.js
@@ -3,7 +3,7 @@ import React from "react";
 function Guest({ image, name, quote, role }) {
   return (
     <div className="mb-20 space-y-4 text-center">
-      <h1 className="inline-block text-3xl font-black tracking-tight text-transparent md:p-3 md:pt-0 md:text-4xl animate-text bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-clip-text">
+      <h1 className="inline-block text-3xl font-black tracking-tight text-transparent md:p-3 md:pt-0 md:text-4xl animate-text bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-gray-300 via-fuchsia-600 to-orange-600 bg-clip-text">
         OUR CHIEF GUEST
       </h1>
       <figure className="md:flex rounded-xl p-8 md:p-0 bg-slate-800/40 w-[90%] md:w-[80%] mx-auto overflow-hidden font-poppins hover:scale-105 transition-all ease-in-out duration-300">
@@ -17,7 +17,7 @@ function Guest({ image, name, quote, role }) {
             <p className="text-lg font-medium md:text-justify">{quote}</p>
           </blockquote>
           <figcaption className="font-medium">
-            <div className="text-xl text-blue-500">{name}</div>
+            <div className="text-xl text-purple-600">{name}</div>
             <div className="font-light text-slate-400">{role}</div>
           </figcaption>
         </div>
