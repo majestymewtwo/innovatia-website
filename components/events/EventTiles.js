4e1b70e20838947a523906b23f3ed5509a739d77
Ruthika <ruthirockstar2218@gmail.com>
2023-11-18 08:26:39 +0530

    Superhero shippuden registration closed

--- components/events/EventTiles.js
+++ components/events/EventTiles.js
@@ -136,12 +136,11 @@ const EventTiles = () => {
                   Superhero Shippuden
                 </h1>
                 <p className="mb-3 text-lg italic text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100">
-                  Anime Trivia: Anime, Manga, Otaku, Cosplay, Shōnen, Shōjo,
-                  Seinen, Josei, Slice of Life
+                  REGISTRATIONS CLOSED!
                 </p>
-                <a href="/events/superhero-shippuden">
-                  <button className="rounded-full bg-neutral-900 py-2 px-3.5 font-com text-sm capitalize text-white shadow shadow-black/60 hover:bg-white hover:text-neutral-900">
-                    Register Now!
+                <a href="">
+                  <button className="rounded-full bg-neutral-900 py-2 px-3.5 font-com text-sm capitalize text-white shadow shadow-black/60 ">
+                  REGISTRATIONS CLOSED!
                   </button>
                 </a>
               </div>
