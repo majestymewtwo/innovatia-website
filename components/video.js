69715c424a5e78d5ec1f3ba80d30aae902e52691
majestymewtwo <majestymewtwo@gmail.com>
2023-11-20 14:49:22 +0530

    promo video title style change

--- components/video.js
+++ components/video.js
@@ -1,12 +1,12 @@
 export default function VideoComponent() {
   return (
-    <>
-      <h1 className='inline-block text-3xl font-black tracking-wide text-transparent lg:p-3 lg:pt-0 lg:text-5xl animate-text bg-gradient-to-r from-[#ec4c4c] via-[#EEF296] to-[#9ADE7B] bg-clip-text text-center w-full font-oswald'>
+    <div className='text-center space-y-4'>
+      <h1 className='inline-block text-3xl font-black tracking-tight text-transparent md:p-3 md:pt-0 md:text-5xl animate-text bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-gray-300 via-fuchsia-600 to-orange-600 bg-clip-text'>
         PROMO VIDEO
       </h1>
       <video controls className='w-[90%] mx-auto lg:w-[70%] focus:outline-none'>
         <source src='/promo-video.mp4' />
       </video>
-    </>
+    </div>
   );
 }
