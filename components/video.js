43d1bfeb4d3fc4b76b0c59bc1c9f1292aaa2e467
majestymewtwo <majestymewtwo@gmail.com>
2023-11-20 11:39:44 +0530

    promo video styke change

--- components/video.js
+++ components/video.js
@@ -1,7 +1,12 @@
 export default function VideoComponent() {
   return (
-    <video controls className='w-[90%] mx-auto'>
-      <source src='/promo-video.mp4' />
-    </video>
+    <>
+      <h1 className='inline-block text-3xl font-black tracking-wide text-transparent lg:p-3 lg:pt-0 lg:text-5xl animate-text bg-gradient-to-r from-[#ec4c4c] via-[#EEF296] to-[#9ADE7B] bg-clip-text text-center w-full font-oswald'>
+        PROMO VIDEO
+      </h1>
+      <video controls className='w-[90%] mx-auto lg:w-[70%] focus:outline-none'>
+        <source src='/promo-video.mp4' />
+      </video>
+    </>
   );
 }
